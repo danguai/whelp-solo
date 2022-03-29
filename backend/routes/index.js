67d02fa4959ca4c6ff7feacf578373a8281b978f
Daniel Blanco <db@d-blanco.com>
2022-03-28 21:08:22 -0400

    Login reducer DONE

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,6 +5,34 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+//   S T A T I C   R O U T E S
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static(path.resolve('../frontend/build')));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
 // router.get('/hello/world', (req, res) => {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
 //     res.send('Hello World!');
