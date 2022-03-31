376e4d260c75197f4a87993d7b80879a8119da4b
Daniel Blanco <db@d-blanco.com>
2022-03-31 16:20:01 -0400

    css for login & signup

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -59,12 +59,18 @@ const Navigation = ({ isLoaded }) => {
     }
 
     return (
-        <div>
-            <nav className="navigation__bar">
-                <div className=''>
-                    {sessionLinks}
+        <div className="navigation__bar">
+            <div className='menu__top__left'>
+                <div className='review__navbar'>
+                    Write a Review
                 </div>
-            </nav>
+                <div className='business__navbar'>
+                    Whelp for Business
+                </div>
+            </div>
+            <div className=''>
+                {sessionLinks}
+            </div>
         </div>
     )
 };
